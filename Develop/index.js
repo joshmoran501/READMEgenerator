c91e33d7df64b741c66ba83db8f58740ab687ca2
josh.moran501 <josh.moran501@gmail.com>
2022-07-06 16:03:08 -0400

    starting code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,112 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
+
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: `input`,
+        message: `What is your github username? (Do not include @)`,
+        name: `username`,
+        validate: async function makeRequest (answer) {
+                try {
+                    let response = await fetch(`https://api.github.com/users/${answer}`);
+                    console.log(response.status)
+                    return response.status
+                } catch (err) {
+                    console.log(`Valid username is required`)
+                    return response.status
+                }
+            } 
+    },
+    {
+        type: `input`,
+        message: `What is the application's github repository?`,
+        name: `repo`,
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log(`A valid repoistory is required`)
+            } else return true
+        }
+    },
+    {
+        type: `input`,
+        message: `What is your project's title?`,
+        name: `title`,
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log(`A valid title is required`)
+            } else return true 
+        }
+    },
+    {
+        type: `input`,
+        message: `Project description:`,
+        name: `description`,
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log(`A valid description is required`)
+            } else return true 
+        }
+    },
+    {
+        type: `input`,
+        message: `Installation instructions:`,
+        name: `installation`,
+    },
+    {
+        type: `input`,
+        message: `Usage information:`,
+        name: `usage`,
+    },
+    {
+        type: `list`,
+        message: `What license would you like to use?`,
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense', 'ISC License'],
+        name: `license`,
+    },
+    {
+        type: `input`,
+        message: `Contribution guidelines:`,
+        name: `contributing`,
+    },
+    {
+        type: `input`,
+        message: `Include any tests written for your application:`,
+        name: `tests`,
+    },
+    {
+        type: `input`,
+        message: `email:`,
+        name: `email`,
+        validate: function (answer) {
+            if (!answer.includes(`@`) || answer.length < 1) {
+                return console.log(`Invalid email address`) 
+            } else return true
+        }
+    }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err)
+        } else return console.log (`Readme file generated`)
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const responses = await inquirer.prompt(questions);
+    console.log(responses)
+
+    const markdown = generateMarkdown(responses)
+    console.log(markdown)
+
+    writeToFile(`ExampleREADme.md`, markdown)
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,109 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseArray = [
+  {
+    name:`GNU AGPLv3`,
+    url: `https://opensource.org/licenses/AGPL-3.0`
+  },
+  {
+    name:`GNU GPLv3`,
+    url: `https://opensource.org/licenses/GPL-3.0`
+  },
+  {
+    name:`GNU LGPLv3`,
+    url: `https://opensource.org/licenses/LGPL-3.0`
+  },
+  {
+    name:`Mozilla Public License 2.0`,
+    url: `https://opensource.org/licenses/MPL-2.0`
+  },
+  {
+    name:`Apache License 2.0`,
+    url: `https://opensource.org/licenses/Apache-2.0`
+  },
+  {
+    name:`MIT License`,
+    url: `https://opensource.org/licenses/MIT`
+  },
+  {
+    name:`Boost Software License 1.0`,
+    url: `https://opensource.org/licenses/BSL-1.0`
+  },
+  {
+    name:`The Unlicense`,
+    url: `https://opensource.org/licenses/unlicense`
+  },
+  {
+    name:`ISC License`,
+    url: `https://opensource.org/licenses/ISC`
+  },
+]
+
+
+let license = data.license
+
+function renderLicenseBadge () {
+  if (license !== ``) {
+    markdown +=   `[![GitHub license](https://img.shields.io/github/license/${data.username}/${data.repo})]`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink() {
+  if (license !== ``) {
+    licenceIndex=licenseArray.indexOf(license)
+    markdown += licenseArray[licenceIndex].url
+  } else return ``
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (data.license !== ``) {
+    markdown += `## License: ${license}`
+  } else return ``
+  renderLicenseLink()
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let contents = `## Table of contents`
+    if (data.installation !== ``) {
+      contents += `[Installation](#installation)`
+    };
+    if (data.usage !== ``) {
+      contents += `[Usage](#usage)`
+    };
+    if (data.contributing !== ``) {
+      contents += `[Contributing](#contributing)`
+    };
+    if (data.license !== ``) {
+      contents += `[License](#License)`
+    };
 
-`;
+  let markdown = 
+  `# ${data.title}`
+
+  renderLicenseBadge()
+  
+  `## Description
+  
+  ${data.description}`
+
+  markdown += contents
+  
+  if (data.installation !== ``) {
+    markdown += `## Installation\
+    ${data.installation}`
+  }
+
+  if (data.usage !== ``) {
+    markdown += `## Usage\
+    ${data.usage}`
+  }
+
+  renderLicenseSection()
 }
 
 module.exports = generateMarkdown;
